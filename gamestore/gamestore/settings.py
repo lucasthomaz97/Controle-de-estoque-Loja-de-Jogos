9469b019bfcd7ee008b6138bfd2da054e83d1ad0
Lucas <lucasthomaz97@gmail.com>
2023-10-11 11:58:49 -0300

    to_prod

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0@#^n84shwuc7cl_sl#j6enzg*7)@)form*q*7ytei6@h)q!1a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'mp7Zc5XdCqyANqzWVidk',
+        'HOST': 'containers-us-west-165.railway.app',
+        'PORT': '7638',
     }
 }
 
@@ -126,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
--- gamestore/gamestore/urls.py
+++ gamestore/gamestore/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home, salvar, editar, alterar, apagar, registrar
 
 urlpatterns = [
@@ -10,3 +12,6 @@ urlpatterns = [
     path('apagar/<int:id>', apagar, name='apagar'),
     path('registrar/', registrar, name='registrar')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
