2adb61b3feaca679641c5f160be110b4e4ca095e
Lucas <lucasthomaz97@gmail.com>
2023-10-11 13:13:50 -0300

    'retry-6'

--- gamestore/gamestore/urls.py
+++ gamestore/gamestore/urls.py
@@ -12,6 +12,3 @@ urlpatterns = [
     path('apagar/<int:id>', apagar, name='apagar'),
     path('registrar/', registrar, name='registrar')
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
